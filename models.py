60bc6af9a7bb8562dea923b46676f171ef7c6402
skywalker290 <dev.ayusshh@gmail.com>
2025-04-21 00:24:12 +0530

    latest

--- models.py
+++ models.py
@@ -57,7 +57,7 @@ def random_forest(X, y):
 def support_vector_machine(X, y):
     X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
 
-    model = SVC(kernel='rbf', probability=True, verbose=True)  # Enable progress output
+    model = SVC(kernel='rbf', probability=True)  # Enable progress output
     model.fit(X_train, y_train)
 
     y_pred = model.predict(X_test)
